6dd8ebb2d416f47632a18d857bb5dcc9c67c5bf7
GSacata <gsacata@gmail.com>
2024-08-07 20:29:39 -0300

    creating some database files 1

---
+++ src/main/java/com/example/demo/dao/PersonDataAccessService.java
@@ -0,0 +1,36 @@
+package com.example.demo.dao;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import com.example.demo.model.Person;
+
+public class PersonDataAccessService implements PersonDao {
+
+    @Override
+    public int deletePersonById(UUID id) {
+        return 0;
+    }
+
+    @Override
+    public int insertPerson(UUID id, Person person) {
+        return 0;
+    }
+
+    @Override
+    public List<Person> selectAllPeople() {
+        return null;
+    }
+
+    @Override
+    public Optional<Person> selectPersonById(UUID id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public int updatePersonById(UUID id, Person person) {
+        return 0;
+    }
+    
+}
